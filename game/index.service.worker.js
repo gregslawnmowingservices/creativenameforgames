b44174855c5b333524305ae8f9b95db41773e808
bandageslmao <146501103+bandageslmao@users.noreply.github.com>
2025-01-31 21:03:40 -0600

    uipdate

--- game/index.service.worker.js
+++ game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738350477|2631151708';
+const CACHE_VERSION = '1738378181|238934066';
 /** @type {string} */
 const CACHE_PREFIX = 'VGdesigncomp-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
